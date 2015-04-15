39d434bb75fbc156453d4cf29afb136776acb29d
Petter Bergman <jon.petter.bergman@gmail.com>
2015-04-15 12:30:16 +0200

    Documentation

--- src/Data/Time/LocalTime/TimeZone/Olson/TH.hs
+++ src/Data/Time/LocalTime/TimeZone/Olson/TH.hs
@@ -1,9 +1,14 @@
-{- |
-Load TimeZoneSeries from an Olson file at compile time.
--}
+-- |
+-- Load TimeZoneSeries from an Olson file at compile time.
+--  
+-- For example:
+--
+-- > myTimeZoneSeries :: TimeZoneSeries
+-- > myTimeZoneSeries = $(loadTZFile "/usr/share/zoneinfo/Europe/Stockholm")
+
 module Data.Time.LocalTime.TimeZone.Olson.TH 
   (
-    loadTZ
+    loadTZFile
   ) where       
 
 import Data.Ratio                          (numerator,
@@ -21,44 +26,45 @@ import Language.Haskell.TH                 (Q,
                                             Lit(..))
 
 -- | Make a splice of a TimeZoneSeries from an Olson file.
---   
---   For example:
---   > myTimeZoneSeries :: TimeZoneSeries
---   > myTimeZoneSeries = $(loadTZ "/usr/share/zoneinfo/Europe/Stockholm")
-loadTZ :: FilePath -- ^ Path to the Olson file.
+loadTZFile :: FilePath -- ^ Path to the Olson file.
        -> Q Exp
-loadTZ zf = runIO $                                                         
-         do
-    TimeZoneSeries def trans <- getTimeZoneSeriesFromOlsonFile zf
-    return $ mkTZS (def,trans)
-    
-mkTZS :: (TimeZone,[(UTCTime,TimeZone)]) -> Exp    
-mkTZS (def,tlist) =    
+loadTZFile zf = 
+  runIO $ fmap mkTZS $ getTimeZoneSeriesFromOlsonFile zf
+
+-- | Make a splice of a TimeZoneSeries.    
+mkTZS :: TimeZoneSeries -- ^ The TimeZoneSeries to be spliced
+      -> Exp    
+mkTZS (TimeZoneSeries def tlist) =    
   AppE (AppE (ConE $ mkName "TimeZoneSeries") (litTimeZone def))
   (mkList tlist)
   
-mkList :: [(UTCTime,TimeZone)] -> Exp  
+mkList :: [(UTCTime,TimeZone)] 
+       -> Exp  
 mkList l = ListE $ map mkPair l    
     
-mkPair :: (UTCTime,TimeZone) -> Exp           
+mkPair :: (UTCTime,TimeZone) 
+       -> Exp           
 mkPair (t,tz) =    
   TupE [litUTCTime t,litTimeZone tz]
     
-litUTCTime :: UTCTime -> Exp  
+litUTCTime :: UTCTime 
+           -> Exp  
 litUTCTime (UTCTime (ModifiedJulianDay d) s) = 
   AppE (AppE (ConE $ mkName "UTCTime")
         (AppE (ConE (mkName "ModifiedJulianDay")) (LitE $ IntegerL $ d)))
   (AppE (VarE $ mkName "secondsToDiffTime") 
    (LitE $ IntegerL $ diffTimeToInteger s))
 
-diffTimeToInteger :: DiffTime -> Integer
+diffTimeToInteger :: DiffTime 
+                  -> Integer
 diffTimeToInteger s =
   let r = toRational s
       n = numerator r
       d = denominator r in
   (n `div` d)
 
-litTimeZone :: TimeZone -> Exp
+litTimeZone :: TimeZone 
+            -> Exp
 litTimeZone (TimeZone m s n) = 
   AppE (AppE (AppE (ConE (mkName "TimeZone")) 
               (LitE $ IntegerL $ toInteger m))
